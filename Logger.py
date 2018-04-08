561095a2a759532db382b4f1aaa8a583e9aecf0b
Jan Kubovy <jan@kubovy.eu>
2018-04-08 16:16:37 +0200

    Update logger.

--- Logger.py
+++ Logger.py
@@ -3,20 +3,35 @@
 #
 # Author: Jan Kubovy (jan@kubovy.eu)
 #
+import os
 import time
+import traceback
 
 
 class Logger:
 
-    def __init__(self, name, debug=False):
+    def __init__(self, name, debug=False, log_file="/var/log/raspi-project.log"):
         self.name = name
         self.isDebugging = debug
+        self.log_file = log_file
+        try:
+            os.remove(log_file)
+        except OSError:
+            traceback.print_exc()
         
     def debug(self, message):
-        if self.isDebugging: print(time.ctime() + " DEBUG [" + self.name + "]: " + message)
+        if self.isDebugging: self.__log__(time.ctime() + " DEBUG [" + self.name + "]: " + message)
 
     def info(self, message):
-        print(time.ctime() + " INFO [" + self.name + "]: " + message)
+        self.__log__(time.ctime() + " INFO [" + self.name + "]: " + message)
         
     def error(self, message):
-        print(time.ctime() + " ERROR [" + self.name + "]: " + message)
+        self.__log__(time.ctime() + " ERROR [" + self.name + "]: " + message)
+
+    def __log__(self, raw_message):
+        print(raw_message)
+        try:
+            with open(self.log_file, "a") as fp:
+                fp.write(raw_message)
+        except:
+            traceback.print_exc()
