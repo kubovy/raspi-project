92146a33fc55e1fc8c53ef4e8b8c741ed50f2a08
Jan Kubovy <jan@kubovy.eu>
2018-12-18 23:42:11 +0100

    Fix module naming in topic

--- modules/MQTT.py
+++ modules/MQTT.py
@@ -40,7 +40,7 @@ class MQTT(ModuleLooper):
     def publish(self, topic, payload=None, qos=0, retrain=False, module=None):
         path = [self.client_id, 'state']
         if module is not None:
-            path.append(module if isinstance(module, basestring) else to_snake_case(type(module).__name__))
+            path.append(module if isinstance(module, basestring) else to_snake_case(type(module).__name__, "-"))
         if topic is not None and topic != "":
             path.append(topic)
         self.logger.debug("Publishing: " + "/".join(path) + " qos=" + str(qos) + ", retain=" + str(retrain) + ": " +
